15b61e4dc68b2c28d9d020104f2dd494290be66c
Cindy <cindy.do10@gmail.com>
2020-11-07 11:47:04 -0800

    defined Card.hpp functions

--- Card.hpp
+++ Card.hpp
@@ -2,6 +2,7 @@
 #define _CARD_H
 
 #include <string>
+#include <iostream>
 using namespace std;
 
 const int size = 63;
@@ -10,11 +11,21 @@ class card {
     private:
         string face;
     public:
-        card();
-        void set_value(string);
-        string getFace();
-        string getCard();
-        void print();
+        card() {
+            face = "";
+        }
+
+        void set_value(string cardType) {
+            face = cardType;
+        }
+
+        string getFace() {
+            return face;
+        }
+
+        void print() {
+            cout << face;
+        }
 };
 
 #endif
