03ad9d83baaea77a6418a5e887e05483198e2d98
unknown <apham125@ucr.edu>
2020-11-07 14:43:32 -0800

    added main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,30 @@
 
 using namespace std;
 
-main() {
+int main() {
+    string playerName;
+    Deck d;
+    cout << "RULES OR SUM" << endl;
+
+    cout << "What is your name?" << endl;
+    cin >> playerName;
+
+    Player p1(playerName);
+    Player p2("Opponent");
+
+    while (p1.getWins() != 3 && p2.getWins() !=3){
+            p1.setHand(d);
+            P2.setHand(d);
+            //code lol
+    }
+
+    if (p1.getWins() == 3){
+        cout << "Congrats! You win!" << endl;
+    }
+    else {
+        cout << "u lost";
+    }
+
 
     
     return 0;
